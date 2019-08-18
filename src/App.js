53b7fd126d01f718876cd4f81555d6e0ea6f156f
Prasun-Acharjee <acharjee.prasun@gmail.com>
2019-08-18 12:19:44 +0530

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={cel:0,fah:32}
+  }
+  changetof=(event)=>
+  {
+    this.setState({cel:event.target.value,fah:9/5*(event.target.value)+32});
+  }
+  changetoc=(event)=>
+  {
+    this.setState({cel:((event.target.value)-32)*5/9,fah:event.target.value});
+  }
+  render(){
+    return(
+      <div>
+        <h1>Celcius</h1>
+        <input type="number" value={this.state.cel} onChange={this.changetof}/>
+        <h1>Fahrenheit</h1>
+        <input type="number" value={this.state.fah} onChange={this.changetoc}/>
+      </div>
+    );
+  }
 }
-
 export default App;
